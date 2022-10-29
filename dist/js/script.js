e9e17c35884bbbd8a3405b6a47e50f5e7a96c962
Halfa <85602339+HalfaChink@users.noreply.github.com>
2022-10-29 23:46:42 +0700

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,29 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// Gallery
+const images = document.querySelectorAll('.img-gallery');
+const imagesLight = document.querySelector('.image-pop');
+const containerLight = document.querySelector('.image-lightbox');
+
+images.forEach(image =>{
+    image.addEventListener('click', ()=>{
+        appearImage(image.getAttribute('src'))
+    
+    })
+})
+
+containerLight.addEventListener('click', (e) =>{
+    if(e.target !== imagesLight){
+        containerLight.classList.remove('show');
+        imagesLight.classList.remove('showImage'); 
+    }
+})
+
+const 
+appearImage = (image) =>{
+    imagesLight.src = image;
+    containerLight.classList.add('show');
+    imagesLight.classList.add('showImage');
+}
